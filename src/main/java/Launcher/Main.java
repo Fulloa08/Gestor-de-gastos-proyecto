16faae2e155dd3c6f4cc16af51ca6e538b8cce3b
Nick <n.molfinqueo01@ufromail.cl>
2025-06-22 00:52:20 -0400

    Primer Commit del Segundo Avance

---
+++ src/main/java/Launcher/Main.java
@@ -0,0 +1,10 @@
+package Launcher;
+
+import controlador.GestorApp;
+
+public class Main {
+    public static void main(String[] args) {
+        GestorApp app = new GestorApp();
+        app.iniciar();
+    }
+}
