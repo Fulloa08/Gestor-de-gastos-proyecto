eb06a718cf3eb367791c7f85560bb7221fbab9de
Fulloa08 <f.ulloa08@ufromail.cl>
2025-06-23 01:59:39 -0400

    código de Main subido

---
+++ src/main/java/Launcher/Main.java
@@ -0,0 +1,10 @@
+package Launcher;
+
+import Controlador.GestorApp;
+
+public class Main {
+    public static void main(String[] args) {
+        GestorApp app = new GestorApp();
+        app.iniciar();
+    }
+}
